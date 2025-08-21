2f762f0351312bef564a9b187dcc55790725ca62
Fred Spielman <spielmanfred@gmail.com>
2025-08-21 19:24:37 -0400

    removed comments explaining instructions (in verification section of README now)

--- pipelined/src/instruction_mem.vhd
+++ pipelined/src/instruction_mem.vhd
@@ -6,8 +6,8 @@ entity instruction_mem is
   port(
 	 address: in std_logic_vector(31 downto 0);
 
-    instruction: out std_logic_vector(31 downto 0)
-	);
+     instruction: out std_logic_vector(31 downto 0)
+  );
 end entity instruction_mem;
 
 architecture instr_mem_arch of instruction_mem is 
@@ -20,34 +20,34 @@ architecture instr_mem_arch of instruction_mem is
 		0 => x"00000013", -- nop
 
 		-- lw x5, 0(x7)
-		1 => x"0003A283",  -- x7=5, loads from byte address 5 -> ram[1]=13 index: 5/4=1 -> x5=13
+		1 => x"0003A283",
 
 		-- add x4, x5, x6
-		2 => x"00628233", -- x4 = 13 + 4 = 17
+		2 => x"00628233", 
 		
-		-- add x7, x4, x8 -- x7 = 17 + 10 = 27
+		-- add x7, x4, x8
 		3 => x"008203b3",
 		
 		-- add x3, x4, x5
-		4 => x"005201b3", -- x3 = 17 + 13 = 30
+		4 => x"005201b3",
 		
 		-- add x16, x17, x18
-		5 => x"01288833", -- x16 = 12 + 6 = 18
+		5 => x"01288833",
 		
 		-- add x15, x19, x16
-		6 => x"010987b3", -- x15 = 7 + 18 = 25
+		6 => x"010987b3",
 		
 		-- add x21, x20, x16
-		7 => x"010a0ab3", -- x21 = 8 + 18 = 26
+		7 => x"010a0ab3",
 		
 		-- beq x30, x31, -32
-		8 => x"ffff00e3", -- x30 = x31 = 15 -> branch taken (jumps back 7 instructions) -> control hazard
+		8 => x"ffff00e3",
 		
 		-- add x15, x19, x16
-		9 => x"010987b3",	-- control hazard -> gets flushed
+		9 => x"010987b3",
 		
 		-- add x21, x20, x16
-		10 => x"010a0ab3", -- control hazard -> gets flushed 
+		10 => x"010a0ab3",
 		
 		others => (others => '0')
 	);
